3762bddb7955ec296afa3ce8e93536eefb9022c8
Lisa Bodner <lisabodner@tsn.at>
2026-03-27 10:35:44 +0100

    Add multiply function

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def add(a, b):
+	return a + b
+
+
+def subtract(a, b):
+	return a - b
+
+
+def multiply(a, b):
+	return a * b
+
+
+if name == ” main ”:
+	print("5 + 3 = ", add(5, 3))
+	print("4 - 1 = ", subtract(4, 1))
+	print("2 * 3 = ", multiply(2, 3))
